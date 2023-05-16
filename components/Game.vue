d293410017a01a48182ec06776c14b726d196fd2
Dr-Vell <davidzamora.2001@hotmail.es>
2023-05-16 16:34:01 +0200

    Fullscreen Game & No news error fixed
    Added a button to set fullscreen on games and fixed an bug for no news situations.

--- components/Game.vue
+++ components/Game.vue
@@ -1,19 +1,46 @@
-<script setup lang="ts">
-    const props = defineProps({
-        title: {
-            type: String,
-            required: true,
-        },
-        route:{
-            type: String,
-            required: true,
-        }
-    })
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  route:{
+    type: String,
+    required: true,
+  },
+});
+
+const game_frame = ref(null);
+
+function toggleFullscreen() {
+  if (game_frame.value) {
+    if (game_frame.value.requestFullscreen) {
+      game_frame.value.requestFullscreen();
+    } else if (game_frame.value.mozRequestFullScreen) {
+      game_frame.value.mozRequestFullScreen();
+    } else if (game_frame.value.webkitRequestFullscreen) {
+      game_frame.value.webkitRequestFullscreen();
+    } else if (game_frame.value.msRequestFullscreen) {
+      game_frame.value.msRequestFullscreen();
+    }
+  }
+}
 </script>
+
 
 <template>
     <div class="contenido"> 
         <h1 class="contenido-titulo">{{ title }}</h1>
-        <iframe :src="route" width="1245" height="700" title="game-frame"></iframe>
+        <div class="iframe-container">
+            <iframe :src="route" ref="game_frame" width="100%" height="100%" title="game-frame" class="iframe-game"></iframe>
+            <button class="fullscreen-button" @click="toggleFullscreen">⛶</button>
+        </div>
     </div>
 </template>
+
+<style>
+
+
+</style>
--- components/News.vue
+++ components/News.vue
@@ -5,25 +5,33 @@
     pending,
   } = await useAsyncData("news", () =>
     $fetch(
-      `https://newsapi.org/v2/everything?q=ciberseguridad&apiKey=24fb20258f5d4ac7b8296b6103e60b34`
+      `https://newsapi.org/v2/everything?q=ciberseguridad&apiKey=d7d1b168421d46baaa9cfd9ae688e744`
     )
   );
 
 </script>
 
 <template>
-    <div class="contenido">
-        <h1 class="contenido-titulo">News</h1> 
-        <template v-if="pending">Loading news...</template>
-        <template v-else>
-            <div v-for="articulo in news.articles" :key="articulo.url" class="articulo">
-                <img :src="articulo.urlToImage" alt="Artículo imagen" class="articulo-imagen">
-                <div class="articulo-info">
-                    <a :href="articulo.url" target="_blank" class="articulo-titulo">{{ articulo.title }}</a>
-                    <p class="articulo-autor">Por {{ articulo.author }} - {{ articulo.publishedAt }}</p>
-                </div>
-            </div>
-        </template>
-    </div>
+  <div class="contenido">
+    <h1 class="contenido-titulo">News</h1> 
+    <template v-if="pending">Loading news...</template>
+    <template v-else>
+      <template v-if="news">
+        <div v-for="articulo in news.articles" :key="articulo.url" class="articulo">
+          <img :src="articulo.urlToImage" alt="Artículo imagen" class="articulo-imagen">
+          <div class="articulo-info">
+            <a :href="articulo.url" target="_blank" class="articulo-titulo">{{ articulo.title }}</a>
+            <p class="articulo-autor">Por {{ articulo.author }} - {{ articulo.publishedAt }}</p>
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <p>Límite de llamadas a las API superado.</p>
+        <p>Espera hasta mañana para ver más.</p> 
+        <p>Perdone las molestias.</p>
+
+      </template>
+    </template>
+  </div>
 </template>
 
