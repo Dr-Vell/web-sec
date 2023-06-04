4addb58a1b6d301216a7dfa32f2d69a3cdd46a21
Dr-Vell <davidzamora.2001@hotmail.es>
2023-06-04 17:26:50 +0200

    Download Buttons Added
    Now there's a download button on each game page to get the Windows version, which is the optimal version of the game, so the player can have a better experience playing them. Also added the .zip files of the games to download. Injection Dungeon .zip is empty since Git doesnt allow 100MB+ files to be uploaded.

--- components/Game.vue
+++ components/Game.vue
@@ -1,36 +1,36 @@
 <script setup>
-import { ref } from 'vue';
+  import { ref } from 'vue';
 
-const props = defineProps({
-  title: {
-    type: String,
-    required: true,
-  },
-  route:{
-    type: String,
-    required: true,
-  }, 
-  showFullscreenButton: {
-    type: Boolean,
-    default: true,
-  },
-});
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+    route:{
+      type: String,
+      required: true,
+    }, 
+    droute:{
+      type: String,
+      required: true,
+    }, 
+  });
 
-const game_frame = ref(null);
+  const game_frame = ref(null);
 
-function toggleFullscreen() {
-  if (game_frame.value) {
-    if (game_frame.value.requestFullscreen) {
-      game_frame.value.requestFullscreen();
-    } else if (game_frame.value.mozRequestFullScreen) {
-      game_frame.value.mozRequestFullScreen();
-    } else if (game_frame.value.webkitRequestFullscreen) {
-      game_frame.value.webkitRequestFullscreen();
-    } else if (game_frame.value.msRequestFullscreen) {
-      game_frame.value.msRequestFullscreen();
+  function toggleFullscreen() {
+    if (game_frame.value) {
+      if (game_frame.value.requestFullscreen) {
+        game_frame.value.requestFullscreen();
+      } else if (game_frame.value.mozRequestFullScreen) {
+        game_frame.value.mozRequestFullScreen();
+      } else if (game_frame.value.webkitRequestFullscreen) {
+        game_frame.value.webkitRequestFullscreen();
+      } else if (game_frame.value.msRequestFullscreen) {
+        game_frame.value.msRequestFullscreen();
+      }
     }
   }
-}
 </script>
 
 
@@ -41,10 +41,7 @@ function toggleFullscreen() {
             <iframe :src="route" ref="game_frame" width="100%" height="100%" title="game-frame" class="iframe-game"></iframe>
             <button class="fullscreen-button" @click="toggleFullscreen">⛶</button>
         </div>
+        <a v-if="droute=='1'" href="/pc-builds/Injection_Dungeon-1.0-pc.zip" :download="title + '.zip'"><button class="download-button">Download Game for Windows</button></a>
+        <a v-if="droute=='2'" href="/pc-builds/Escape_the_B.A.C._Room-1.0-pc.zip" :download="title + '.zip'"><button class="download-button">Download Game for Windows</button></a>
     </div>
 </template>
-
-<style>
-
-
-</style>
--- pages/Escape-The-BAC-Room.vue
+++ pages/Escape-The-BAC-Room.vue
@@ -2,6 +2,7 @@
     <div>
     <Game title="Escape The B.A.C. Room"
           route="games/Escape_The_BAC_Room-1.0-web/index.html" 
+          droute="2"
           />
     </div>
 </template>
