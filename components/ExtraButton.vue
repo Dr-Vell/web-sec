77166ec495fbe1ddef632b44aa91b85362be9e76
Dr-Vell <davidzamora.2001@hotmail.es>
2023-06-05 16:53:54 +0200

    Extras text justified
    Changed text align from center to justify for every description in the Extras page

--- components/ExtraButton.vue
+++ components/ExtraButton.vue
@@ -3,7 +3,7 @@
 
         <div class="extra-button-div">
         <a class= "extra-button" href="https://www.hackthebox.com"><img class= "extra-button-pic" src="/htb.png" alt="Hackthebox.com"></a>
-        <h3>Hack The Box is a gamified cybersecurity upskilling, certification, and talent assessment software platform enabling individuals, businesses, government institutions, 
+        <h3 class="extra-desc">Hack The Box is a gamified cybersecurity upskilling, certification, and talent assessment software platform enabling individuals, businesses, government institutions, 
             and universities to sharpen their offensive and defensive security expertise. A cool solution for cybersecurity professionals and organizations to continuously enhance their 
             cyber-attack readiness by improving their red, blue, and purple team capabilities.
             <br><br>
@@ -13,7 +13,7 @@
 
         <div class="extra-button-div-reverse">            
         <a class= "extra-button" href="https://owasp.org"><img class= "extra-button-pic" src="/owasp.png" alt="Owasp.org"></a>
-        <h3>OWASP is a non-profit organization that focuses on improving the security of software applications. OWASP provides resources, 
+        <h3 class="extra-desc">OWASP is a non-profit organization that focuses on improving the security of software applications. OWASP provides resources, 
             tools, and documentation to help individuals and organizations understand and address web application security issues.
             <br><br>
             OWASP also maintains a list of the top web application security risks, known as the OWASP Top 10, which highlights the most critical vulnerabilities and risks that
@@ -25,7 +25,7 @@
 
         <div class="extra-button-div">
         <a class= "extra-button" href="https://www.educationarcade.co.nz"><img class= "extra-button-pic" src="/ea.png" alt="EducationArcade.co.nz"></a>
-        <h3>Education Arcade is a free cybersecurity awareness resource that uses gamification. Founded by Brock Roderick in 2019. On the website,
+        <h3 class="extra-desc">Education Arcade is a free cybersecurity awareness resource that uses gamification. Founded by Brock Roderick in 2019. On the website,
              there are various games through which anyone can learn about different topics, from common scams to password security.
             <br><br>
             Press the image to visit their website.
@@ -34,7 +34,7 @@
 
         <div class="extra-button-div-reverse">            
         <a class= "extra-button" href="https://picoctf.org"><img class= "extra-button-pic" src="/pico.png" alt="PicoCTF.org"></a>
-        <h3>picoCTF is a free educational program in computer security with original content based on a capture the flag framework created by security and privacy experts 
+        <h3 class="extra-desc">picoCTF is a free educational program in computer security with original content based on a capture the flag framework created by security and privacy experts 
             from Carnegie Mellon University.
             <br><br>
             Press the image to visit their website.
@@ -43,7 +43,7 @@
 
         <div class="extra-button-div">
         <a class= "extra-button" href="https://it.tamu.edu/security/cybersecurity-games/index.php"><img class= "extra-button-pic" src="/texas.png" alt="Texas A&M University"></a>
-        <h3>In 2015, the Technology Services department of Texas A&M University launched a platform that includes a series of online games for the Cybersecurity Awareness Month. 
+        <h3 class="extra-desc">In 2015, the Technology Services department of Texas A&M University launched a platform that includes a series of online games for the Cybersecurity Awareness Month. 
             Each of these games is designed to be entertaining while teaching players how to use the internet in a safer manner. They have been creating a new game every year since
              2015, and all games are accessible for free on their website.
             <br><br>
