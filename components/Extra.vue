bfedb0021e666ab3fbfc58a3c098b744fc04505f
Dr-Vell <davidzamora.2001@hotmail.es>
2023-06-05 15:33:39 +0200

    "Extra" page  Implemented
    This new page shows the user some other alternatives/tools to learn about web security.

--- components/Extra.vue
+++ components/Extra.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="contenido">      
         <h1 class="contenido-titulo">Extra</h1>  
-        <h2 class="contenido-texto">            
-            tba       
-        </h2>  
+        <ExtraButton /> 
     </div>
 </template>
