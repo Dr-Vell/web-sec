07e445d800e00842464f658c71b1d3740c80c87e
Dr-Vell <davidzamora.2001@hotmail.es>
2023-06-14 12:40:33 +0200

    Added API Key to .env
    The API key used to search news is now saved in a .env file.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    runtimeConfig: {
+        public:{
+            newsApiKey: process.env.NEWS_API_KEY,  
+        }      
+      },
     pages: true,
 
     css: ["~/assets/styles.css"],
-      
+
     })
